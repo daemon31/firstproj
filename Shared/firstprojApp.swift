a8cc2622ea51fe73b9aa10cb916b9ef0eb812b05
daemon31 <polodimon@gmail.com>
2022-12-12 13:18:00 +0100

    Initial Commit

---
+++ Shared/firstprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstprojApp.swift
+//  Shared
+//
+//  Created by Dimitri Krylasov on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct firstprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
